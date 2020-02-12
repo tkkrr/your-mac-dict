8198cf3586d1adcb0d51b5cc4d024b6c3ffda6b5
tkkrr <reioffitial@gmail.com>
2020-02-12 19:12:02 +0900

    delete comment

--- extension.js
+++ extension.js
@@ -1,10 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const execSync = require('child_process').execSync
 const path = require('path')
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 
 
@@ -48,14 +44,8 @@ const getSearchPhrase = () => {
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "your-mac-dict" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.dict', function () {
 		// The code you place here will be executed every time your command is executed
 		// const child = exec(
@@ -77,7 +67,6 @@ function activate(context) {
 		const cmd = ["python3", pypath, word].join(" ")
 
 		const child = execSync(cmd).toString()
-		// Display a message box to the user
 		vscode.window.showInformationMessage(child);
 	});
 
@@ -85,7 +74,6 @@ function activate(context) {
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
